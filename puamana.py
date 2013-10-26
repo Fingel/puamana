147429d752565751fcbd0e2b40d59c3caedb274d
Austin Riba <root@austinriba.com>
2013-10-26 13:44:43 -0700

    default to showing links from last tuesday

--- puamana.py
+++ puamana.py
@@ -3,14 +3,22 @@ from flask import render_template, redirect, url_for
 from flask import request
 from flask.ext.pymongo import PyMongo
 import datetime
+from datetime import date, timedelta
 
 app = Flask(__name__)
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    links = mongo.db.data.find().sort('date', -1);
-    return render_template('index.html', links=links)
+    today =  datetime.datetime.now();
+    offset = (today.weekday() - 1) % 7
+    last_tuesday = today - timedelta(days=offset)
+    links = mongo.db.data.find({"date" : {"$gte": last_tuesday}}).sort('date', -1);
+    return render_template('index.html', links=links, last=last_tuesday)
+
+@app.route('/all')
+def all():
+  return render_template('index.html', links=mongo.db.data.find().sort('date', -1));
 
 @app.route('/add', methods=['POST', 'GET'])
 def add():
@@ -19,7 +27,7 @@ def add():
     else:
       data = {"url": request.form['url'],
               "description": request.form['description'],
-              "date": datetime.datetime.utcnow()}
+              "date": datetime.datetime.now()}
       mongo.db.data.insert(data);
       return redirect(url_for('index'))
 
